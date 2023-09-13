1bc2c21737805f75a1b08318c16dd3b4e1080d8b
brandon <brandon@localhost>
2023-09-13 15:48:34 -0500

    3:48 Studio 2

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -6,19 +6,29 @@ public class Ruin {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		System.out.println("Start amount: ");
 		int startAmount = in.nextInt();
+		System.out.println("Win Chance: ");
 		double winChance = in.nextDouble();
+		System.out.println("Win limit: ");
 		double winLimit = in.nextDouble();
 		int currentAmount = startAmount;
+		
 		
 		while (currentAmount > 0 && currentAmount < winLimit) {
 		double Chance = Math.random(); 
 		if (Chance < winChance) {
 			currentAmount = currentAmount + 1;
 			System.out.println("You won");
+			System.out.println("Current amount: " + "$" + currentAmount);
 		}
 		if (Chance > winChance) {
 			currentAmount = currentAmount - 1;
 			System.out.println("You lost");
+			System.out.println("Current amount: " + "$" + currentAmount);
+			
+			// Simulations
+			
+			
 		}
 		}}}
